1e994c5b9c7c2ac218da47abf6fd30ae7c1d72d8
smw22 <sergiomoronwestergaard@gmail.com>
2026-04-18 21:11:38 +0200

    feat:  Implement auth guards in root layout with different auth routes and theme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,50 @@
-import { Stack } from "expo-router";
+import { DarkTheme, DefaultTheme } from "@react-navigation/native";
 import ThemeProvider from "./src/context/ThemeProvider";
 
-export default function Layout() {
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
+
+import { SplashScreenController } from "@/components/SplashScreenController";
+
+import { useAuthContext } from "@/hooks/use-auth-context";
+import AuthProvider from "@/providers/auth-provider";
+
+// Separate RootNavigator so we can access the AuthContext
+function RootNavigator() {
+  const { isLoggedIn } = useAuthContext();
+
+  return (
+    <Stack>
+      {/* TODO - add protected routes here
+      <Stack.Protected guard={isLoggedIn}>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack.Protected> */}
+      <Stack.Protected guard={!isLoggedIn}>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack.Protected>
+
+      <Stack.Protected guard={!isLoggedIn}>
+        <Stack.Screen name="login" options={{ headerShown: false }} />
+      </Stack.Protected>
+
+      <Stack.Protected guard={!isLoggedIn}>
+        <Stack.Screen name="signup" options={{ headerShown: false }} />
+      </Stack.Protected>
+      {/* TODO - add 404 page
+      <Stack.Screen name="+not-found" /> */}
+    </Stack>
+  );
+}
+
+export default function RootLayout() {
   return (
     <ThemeProvider>
-      <Stack />
+      <AuthProvider>
+        <SplashScreenController />
+        <RootNavigator />
+        <StatusBar style="auto" />
+      </AuthProvider>
     </ThemeProvider>
   );
 }
