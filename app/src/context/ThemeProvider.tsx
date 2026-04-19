3cfcf508805075c890274fffe64186c7535376b9
MikkelRuby <eaa23miru@students.eaaa.dk>
2026-04-19 19:39:47 +0200

    feat: update theme structure to include colors object and enhance type definitions

--- app/src/context/ThemeProvider.tsx
+++ app/src/context/ThemeProvider.tsx
@@ -3,7 +3,11 @@ import { useColorScheme } from "react-native";
 import { darkTheme, lightTheme } from "../theme/theme";
 import { ThemeContext } from "./ThemeContext";
 
-export default function ThemeProvider({ children }) {
+export default function ThemeProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const colorScheme = useColorScheme();
 
   const theme = colorScheme === "dark" ? darkTheme : lightTheme;
