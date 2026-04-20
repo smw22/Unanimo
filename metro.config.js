d09044cc0787d3a00827302af9f1af2f1b0784e2
smw22 <sergiomoronwestergaard@gmail.com>
2026-04-20 13:04:08 +0200

    fix metroconfig nativewind for windows

--- metro.config.js
+++ metro.config.js
@@ -8,12 +8,39 @@ const config = getDefaultConfig(__dirname);
 // Add wasm asset support
 config.resolver.assetExts.push("wasm");
 
+// Ignore optional prebuilt Lightning CSS binaries for non-Windows platforms.
+// On some Windows setups Metro may attempt to watch these missing package paths.
+const lightningCssNonWindowsBinaries =
+  /node_modules[\\/](lightningcss-(darwin|linux|android|freebsd)[^\\/]*)([\\/].*)?$/;
+
+const existingBlockList = config.resolver.blockList;
+if (!existingBlockList) {
+  config.resolver.blockList = [lightningCssNonWindowsBinaries];
+} else if (Array.isArray(existingBlockList)) {
+  config.resolver.blockList = [
+    ...existingBlockList,
+    lightningCssNonWindowsBinaries,
+  ];
+} else {
+  config.resolver.blockList = [
+    existingBlockList,
+    lightningCssNonWindowsBinaries,
+  ];
+}
+
 // Add COEP and COOP headers to support SharedArrayBuffer
+config.server = config.server ?? {};
+const previousEnhanceMiddleware = config.server.enhanceMiddleware;
+
 config.server.enhanceMiddleware = (middleware) => {
+  const baseMiddleware = previousEnhanceMiddleware
+    ? previousEnhanceMiddleware(middleware)
+    : middleware;
+
   return (req, res, next) => {
     res.setHeader("Cross-Origin-Embedder-Policy", "credentialless");
     res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-    middleware(req, res, next);
+    return baseMiddleware(req, res, next);
   };
 };
 
